50b321df26fcc830e27d5d570fdaf48961e3790b
Marcus Graf von Wilamowitz-Moellendorff <marcus@wilamowitz.info>
2021-01-05 18:45:44 +0100

    Rework

--- PAG.h
+++ PAG.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "../../version.h"
 #include <Arduino.h>
 /**
  * Abstract Class for GUI Page
@@ -9,6 +10,20 @@ typedef struct
     int16_t x;
     int16_t y;
 } PAG_pos_t;
+#define PAGE_WIDTH 320
+#define PAGE_HEIGHT 240
+#ifdef HW_M5PAPER
+    #define PAG_BLACK 15 
+    #define PAG_WHITE 0
+    #define PAG_FOREGND PAG_BLACK
+    #define PAG_BACKGND PAG_WHITE
+#endif
+#ifdef HW_M5CORE2
+    #define PAG_BLACK 0x0000
+    #define PAG_WHITE 0xFFFF
+    #define PAG_FOREGND PAG_WHITE
+    #define PAG_BACKGND PAG_BLACK
+#endif
 
 class Page
 {
